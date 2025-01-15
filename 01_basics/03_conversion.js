6b8ceca9d32c54e3119cd978fb7f8ced1e2a8ea6
Naitikgupta01 <guptanaitik467@gmail.com>
2025-01-15 08:37:53 +0000

    Conversion

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,51 @@
+let score = "33"
+
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+//  "33abc" => NaN(Not a number)
+// Null => 0
+//  true => 1 or false => 0
+
+let isLoggedIn=1
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+let someNumber=33
+
+// let stringNumber=String(someNumber);
+// console.log(stringNumber)
+
+// ******************operartions********************
+
+let value=3
+let negValue=-value
+// console.log(negValue);
+
+let str1 = "Hello"
+let str2 = " Naitik"
+let str3 = str1+str2
+console.log(str3);
+
+// console.log("1"+2+2);// if there is string first then all will be treated as string
+// console.log(1+2+"2");
+
+// console.log(+true);
+// console.log(+"");
+
+let Gamechanger = 100
+++Gamechanger
+console.log(Gamechanger);
+
+
+
+
+
+
+

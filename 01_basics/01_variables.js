f2ca26b09a9ccdee7cf95eaf181728475c7d3899
Naitikgupta01 <guptanaitik467@gmail.com>
2025-01-15 08:24:44 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 122445
+let accountEmail = "naitik@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+let accountState;
+// accountId = 2
+accountEmail = "hc@hc.com"
+accountPassword = "2121212"
+accountCity = "New zealand"
+console.log(accountId);
+console.log(accountEmail);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+/*
+Prefered not to use var
+because of issue in function scope
+*/

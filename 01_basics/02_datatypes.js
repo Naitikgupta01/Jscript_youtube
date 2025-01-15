8dfa0edeadb69c62fe5e37a3e73c80a04303079c
Naitikgupta01 <guptanaitik467@gmail.com>
2025-01-15 08:24:57 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict";//treat all JS code as newed version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3 + 3)
+// code readability should be high
+console.log("Naitik")
+
+let name = "Naitik"
+let age = 18
+let isLoggedIn = false
+console.log(age)
+
+// number => 2 to power 53
+// bigint 
+// string=> ""
+// boolean => true/false
+// null => standalone value
+// undefined 
+// symbol => unique
+
+// object
+
+// console.log(typeof "naitik");
+console.log(typeof null);

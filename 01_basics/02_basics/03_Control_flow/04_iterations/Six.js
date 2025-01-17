4e6aeea39a11ce78de3ea1ffef1213bda8839701
Naitikgupta01 <guptanaitik467@gmail.com>
2025-01-17 09:09:12 +0000

    filter map

---
+++ 01_basics/02_basics/03_Control_flow/04_iterations/Six.js
@@ -0,0 +1,26 @@
+// const coding = ["js","python","java","cpp"]
+
+// const values = coding.forEach((item) => {
+//     // console.log(item);
+//     return item
+// })
+
+// console.log(values);
+
+ const MyNums = [1,2,3,4,5,6,7,8,9,10]
+// const newNums = MyNums.filter((num) => {
+//    return num > 4
+// }) 
+
+// const newNum = []
+
+// MyNums.forEach( (num) => {
+//     if (num > 4) {
+//         newNum.push(num)
+//     }
+// })
+// console.log(newNum);
+
+
+
+

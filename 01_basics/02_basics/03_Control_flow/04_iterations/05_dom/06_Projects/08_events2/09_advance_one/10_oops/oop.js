e63407705486af1a3282217194c4ba83aee32eaf
Naitikgupta01 <guptanaitik467@gmail.com>
2025-01-24 14:27:39 +0000

    OOP's concept

---
+++ 01_basics/02_basics/03_Control_flow/04_iterations/05_dom/06_Projects/08_events2/09_advance_one/10_oops/oop.js
@@ -0,0 +1,32 @@
+// const user ={
+    // username: "hitesh",
+    // loginCount: 8,
+    // signedIn: true,
+
+    // getUserDetails: function(){
+        // console.log("Got user details from database");
+        // console.log(`Username: ${this.username}`);
+        // console.log(this);
+        
+    // }
+// }
+
+// console.log(user.username);
+// console.log(user.getUserDetails());
+// console.log(this);
+
+// console.log(user.username);
+// console.log(user.getUserDetails());
+// console.log(this);
+
+function user(username, loginCount, isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = this.isLoggedIn
+    return this
+}
+const userOne =new user("Hitesh", 12, true)
+const userTwo =new user("ChaiAurCode", 18 ,true)
+console.log(userOne);
+console.log(userTwo);
+
